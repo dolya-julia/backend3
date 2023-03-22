8f4e45c6c4f71a32ac2aef115c25ff3c6f0e5c4d
dolya-julia <113310097+dolya-julia@users.noreply.github.com>
2023-03-22 18:58:20 +0300

    MajorUpdate

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 // Отправляем браузеру правильную кодировку,
 // файл index.php должен быть в кодировке UTF-8 без BOM.
 header('Content-Type: text/html; charset=UTF-8');
-
+$user = 'u53298'; 
+$pass = '8737048'; 
+$db = new PDO('mysql:host=localhost;dbname=u53298', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
 // В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
 // и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+ 
   // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
   if (!empty($_GET['save'])) {
     // Если есть параметр save, то выводим сообщение пользователю.
@@ -16,12 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   // Завершаем работу скрипта.
   exit();
 }
-
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
-
 // Проверяем ошибки.
 $errors = FALSE;
-if (empty($_POST['name'])) {
+if (empty($_POST['name']))  {
   print('Заполните имя.<br/>');
   $errors = TRUE;
 }
@@ -37,63 +38,32 @@ if (!preg_match('/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>
   print('Заполните email.<br/>');
   $errors = TRUE;
 }
-// if (!preg_match('/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})$/', $_POST['date']))
-   
-// if (empty($_POST['year']) || !is_numeric($_POST['year']) || !preg_match('/^\d+$/', $_POST['year'])) {
-//   print('Заполните год.<br/>');
-//   $errors = TRUE;
-// }
-
-
-// *************
-// Тут необходимо проверить правильность заполнения всех остальных полей.
-// *************
-
 if ($errors) {
   // При наличии ошибок завершаем работу скрипта.
   exit();
 }
-
 // Сохранение в базу данных.
-
-$user = 'u53298'; // Заменить на ваш логин uXXXXX
-$pass = '8737048'; // Заменить на пароль, такой же, как от SSH
-$db = new PDO('mysql:host=localhost;dbname=u53298', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); // Заменить test на имя БД, совпадает с логином uXXXXX
-
 // Подготовленный запрос. Не именованные метки.
 try {
-    $stmt = $db->prepare("INSERT INTO application (name, email, data, gender, limbs, biog) VALUES('{$_POST['name']}', '{$_POST['email']}', '{$_POST['date']}', '{$_POST['gender']}', '{$_POST['limbs']}','{$_POST['biog']}')");
+    $stmt = $db->prepare("INSERT INTO application (name, email, data, gender, limbs, biog)
+     VALUES('{$_POST['name']}', '{$_POST['email']}', '{$_POST['date']}', '{$_POST['gender']}', '{$_POST['limbs']}','{$_POST['biog']}')");
     $stmt->execute([$_POST['name'], $_POST['email'], $_POST['date'], $_POST['gender'], $_POST['limbs'], $_POST['biog']]);
+    $stmt = $db->prepare("SELECT LAST_INSERT_ID()  as AppId");
+    $stmt->execute();
+    $result = $stmt->fetch();
+    $sql = 'INSERT INTO ap_ab(AppID, AbId) VALUES(:AppID, :AbId)';
+    $stmt = $db->prepare($sql);
+    foreach($_POST['abilities'] as $ability)
+    {
+        $row = [
+              'AppID' => $result["AppId"],
+              'AbId' =>  $ability
+        ];
+        $stmt->execute($row); 
+    }
   }
   catch(PDOException $e){
     print('Error : ' . $e->getMessage());
     exit();
   }
-foreach($_POST['abilities'] as $ability){
-  $stmt = $db->prepare("INSERT INTO ability(AbName) VALUES('$ability')");
-  $stmt->execute(['$ability']);
-}
-
-
-//  stmt - это "дескриптор состояния".
- 
-//  Именованные метки.
-//$stmt = $db->prepare("INSERT INTO test (label,color) VALUES (:label,:color)");
-//$stmt -> execute(['label'=>'perfect', 'color'=>'green']);
- 
-//Еще вариант
-/*$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-$stmt->bindParam(':firstname', $firstname);
-$stmt->bindParam(':lastname', $lastname);
-$stmt->bindParam(':email', $email);
-$firstname = "John";
-$lastname = "Smith";
-$email = "john@test.com";
-$stmt->execute();
-*/
-
-// Делаем перенаправление.
-// Если запись не сохраняется, но ошибок не видно, то можно закомментировать эту строку чтобы увидеть ошибку.
-// Если ошибок при этом не видно, то необходимо настроить параметр display_errors для PHP.
 header('Location: ?save=1');
